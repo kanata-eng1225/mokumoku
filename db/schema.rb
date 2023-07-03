16b47c915fa262523afab85ea8a951f1ee25e993
kanata-eng1225 <murasanv1507@gmail.com>
2023-07-04 00:41:19 +0900

    指摘箇所修正(複合ユニーク制約)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_02_160522) do
+ActiveRecord::Schema.define(version: 2023_07_03_151451) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2023_07_02_160522) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "user_notification_timings", force: :cascade do |t|
